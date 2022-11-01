f5b1b8e898b6d832c180816f949d38ca520d3a0a
Bruno Millalipe <tmbrian177@gmail.com>
2022-11-01 20:21:59 -0300

    first commit

--- src/state/useCart.ts
+++ src/state/useCart.ts
@@ -9,21 +9,7 @@ interface ICart {
 }
 
 export const useCart = create<ICart>((set, get) => ({
-   cart: [
-      {
-         id: 3,
-         title: "Men's Raven Lightweight Zip Up Bomber Jacket",
-         description:
-            "Introducing the Tesla Raven Collection. The Men's Raven Lightweight Zip Up Bomber has a premium, modern silhouette made from a sustainable bamboo cotton blend for versatility in any season. The hoodie features subtle thermoplastic polyurethane Tesla logos on the left chest and below the back collar, a concealed chest pocket with custom matte zipper pulls and a french terry interior. Made from 70% bamboo and 30% cotton.",
-         images: [{ original: 'products/1740250-00-A_0_2000.jpg' }, { original: 'products/1740250-00-A_1.jpg' }],
-         type: 'shirts',
-         price: 130,
-         inStock: 10,
-         slug: 'men_raven_lightweight_zip_up_bomber_jacket',
-         tags: ['shirt'],
-         gender: 'men',
-      },   
-   ],
+   cart: [],
    subTotal: 0,
 
    addProductToCart: product => {
